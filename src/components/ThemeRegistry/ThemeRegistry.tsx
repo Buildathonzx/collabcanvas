d7d12134cfc8e2a945fcbb2f5697c714050baff1
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-02-15 23:38:31 +0100

    🛂 Commit message: Refactor ThemeRegistry component to use TypeScript type hints for better readability and maintainability.

--- src/components/ThemeRegistry/ThemeRegistry.tsx
+++ src/components/ThemeRegistry/ThemeRegistry.tsx
@@ -1,16 +1,15 @@
+'use client';
 
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ReactNode } from 'react';
+import theme from '../../theme/theme';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-}  );    </ThemeProvider>      {children}      <CssBaseline />    <ThemeProvider theme={theme}>  return (export default function ThemeRegistry({ children }: { children: ReactNode }) {import theme from '../../theme/theme';import { ReactNode } from 'react';import CssBaseline from '@mui/material/CssBaseline';import { ThemeProvider } from '@mui/material/styles';'use client';
+export default function ThemeRegistry({ children }: { children: ReactNode }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+}
